72d808d243424c9ffde87f142a9c8e4ac4be134e
user <viktornjoroge@gmail.com>
2026-08-14 20:34:33 +0300

    feat:implemented Bresenhams line algorithm

--- canvas.h
+++ canvas.h
@@ -25,5 +25,6 @@ void set_pixel(Canvas *c, int x, int y, Pixel color);
 int save_ppm(const Canvas *c, const char *filename);
 // Line drawing: Attempt #1 (Naive Interpolation)
 void draw_line_naive(Canvas *c, int x0, int y0, int x1, int y1, Pixel color);
-
+// Line drawing: Attempt #3 (Bresenham's Integer Algorithm)
+void draw_line_bresenham(Canvas *c, int x0, int y0, int x1, int y1, Pixel color);
 #endif // CANVAS_H
